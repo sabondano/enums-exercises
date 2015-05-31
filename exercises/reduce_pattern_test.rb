525f2d17cce82ef160f8f869e3b54d2fa8ee532f
Sebastian Abondano <sabondano1@gmail.com>
2015-05-30 23:22:14 -0600

    first draft

--- exercises/reduce_pattern_test.rb
+++ exercises/reduce_pattern_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 
 class ReducePatternTest < Minitest::Test
 
-  def test_sum_a_list_of_numbers
+	def test_sum_a_list_of_numbers
     numbers = [32, 1, 21, 5, 81, 333]
     sum = 0
     numbers.each do |number|
@@ -17,48 +17,62 @@ class ReducePatternTest < Minitest::Test
     numbers = [28, 12, 38, 1, 91]
     difference = 0
     numbers.each do |number|
-      # Your code goes here
+      difference -= number
     end
     assert_equal -170, difference
   end
 
   def test_multiply_list_of_numbers
-    skip
     numbers = [2, 3, 5, 7]
     product = 1
-    # Your code goes here
+    numbers.each do |number|
+			product *= number
+		end
     assert_equal 210, product
   end
 
   def test_capitalize_keywords_in_phrase_one_fish_two_fish_red_fish_blue_fish
-    skip
     keywords = ["fish", "blue"]
     phrase = 'one fish two fish red fish blue fish'
-    # Your code goes here
+		phrase = phrase.split(" ")
+    phrase.each_with_index do |word, index|
+			if word == keywords[0] || word == keywords[1]
+				phrase[index] = word.upcase
+			end
+		end
+		phrase = phrase.join(" ")
     assert_equal 'one FISH two FISH red FISH BLUE FISH', phrase
   end
 
   def test_divide_560_by_a_bunch_of_numbers
-    skip
     numbers = [2, 2, 2, 5, 7]
     quotient = 560
-    # Your code goes here
+    numbers.each do |number|
+			quotient /= number
+		end
     assert_equal 2, quotient
   end
 
+
   def test_subtract_smallest_values_from_100
-    skip
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     difference = 100
-    # Your code goes here
+    elements.each do |element|
+			smallest = element[0]
+			element.each do |number|
+				smallest = number if number < smallest
+			end
+			difference -= smallest
+		end
     assert_equal 88, difference
   end
 
   def test_add_all_the_second_values_together
-    skip
-    elements = [["a", 1], ["b", 9], ["c", 21]]
+		elements = [["a", 1], ["b", 9], ["c", 21]]
     sum = 0
-    # Your code goes here
+    elements.each do |element|
+			sum += element[1]
+		end
     assert_equal 31, sum
   end
 
